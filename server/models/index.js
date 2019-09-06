03194022759760b70aece7d0cb5f09b1fc05941d
Jajabenit250 <mail@jajabenit.com>
2019-09-06 04:59:15 +0200

    adding-swagger-documentation [finishes #168333595]

--- server/models/index.js
+++ server/models/index.js
@@ -28,63 +28,10 @@ const users = [
     address: 'TORONTO',
     role: 'mentee',
     isAdmin: false
-  },
-  {
-    id: 3,
-    email: 'jaja@gmail.com',
-    firstName: 'HAVUGIMANA',
-    lastName: 'BENIT',
-    password: '$2b$10$hwkOJznThaAoSN3KkntBFO7/BazavJU1BYh6lvemXa33/4t0.UAdu',
-    birthdate: '12/5/1996',
-    occupation: 'student',
-    expertise: 'software',
-    bio: 'inspire the next',
-    phoneNumber: ' +250782314242',
-    address: 'GACYAMO',
-    role: 'mentee',
-    isAdmin: false
-  },
-  {
-    id: 4,
-    email: 'robert@gmail.com',
-    firstName: 'MUHIRE',
-    lastName: 'ROBERT',
-    password: '$2b$10$hwkOJznThaAoSN3KkntBFO7/BazavJU1BYh6lvemXa33/4t0.UAdu',
-    birthdate: '12/5/1996',
-    occupation: 'student',
-    expertise: 'software',
-    bio: 'inspire the next',
-    phoneNumber: ' +250782314242',
-    address: 'RUTURUSU',
-    role: 'mentee',
-    isAdmin: false
-  },
-  {
-    id: 5,
-    email: 'sandrine@gmail.com',
-    firstName: 'UMWALI',
-    lastName: 'SANDRINE',
-    password: '$2b$10$hwkOJznThaAoSN3KkntBFO7/BazavJU1BYh6lvemXa33/4t0.UAdu',
-    birthdate: '12/5/1996',
-    occupation: 'student',
-    expertise: 'software',
-    bio: 'inspire the next',
-    phoneNumber: ' +250782314242',
-    address: 'RUTURUSU',
-    role: 'mentor',
-    isAdmin: false
   }
 ];
 
 let sessions = [
-  {
-    id: 2,
-    mentorId: '5',
-    menteeId: '2',
-    questions: 'i like explore new things',
-    menteeEmail: 'eric@gmail.com',
-    status: 'pending'
-  },
   {
     id: 1,
     mentorId: '3',
